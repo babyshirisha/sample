8b2a7aa4eedea7624c324b4501cd2b9e0562a10b
babyshirisha <99061755+babyshirisha@users.noreply.github.com>
2022-02-11 13:14:55 +0530

    Update armstrong.sh

--- armstrong.sh
+++ armstrong.sh
@@ -6,17 +6,17 @@ co=0
 sum=0
 while [ $n -gt $co ]
 do
-r=$((n % 10))
-i=$((r * r * r))
-sum=$((sum + i))
-n=$((n / 10))
+  r=$((n % 10))
+  i=$((r * r * r))
+  sum=$((sum + i))
+  n=$((n / 10))
 done
 echo $sum
 if [ $sum -eq $amstrong ]
 then
-echo Amstrong number
+  echo Amstrong number
 else
-echo Not an Armstrong number
+  echo Not an Armstrong number
 fi
 }
 result= Amstrong $n
