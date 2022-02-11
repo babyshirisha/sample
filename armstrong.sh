26c2657a7babe6eae84cf242fd46489c47546803
unknown <babyshirisha24@gmail.com>
2022-02-11 12:44:03 +0530

    to find armstrong number

---
+++ armstrong.sh
@@ -0,0 +1,23 @@
+read n
+function Amstrong
+{
+amstrong=$n
+co=0
+sum=0
+while [ $n -gt $co ]
+do
+r=$((n % 10))
+i=$((r * r * r))
+sum=$((sum + i))
+n=$((n / 10))
+done
+echo $sum
+if [ $sum -eq $amstrong ]
+then
+echo Amstrong number
+else
+echo Not an Armstrong number
+fi
+}
+result= Amstrong $n
+echo "$result"
